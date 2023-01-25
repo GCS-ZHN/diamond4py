36feeffd6bbe761a1f4ac9094b3c8bdba2099221
Zhang.H.N <zhang.h.n@foxmail.com>
2023-01-25 20:38:15 +0800

    update test case

--- test/test.py
+++ test/test.py
@@ -1,17 +1,16 @@
-import sys
-from os.path import dirname
-sys.path.append(dirname(dirname(__file__)))
+import os
 from diamond4py import Diamond
 
 diamond = Diamond(
-    database="test/cafa4.dmnd",
+    database="cafa4.dmnd",
     n_threads=4,
 )
 print(diamond.version)
+if not os.path.exists("cafa4.dmnd"):
+    diamond.makedb("test_proteins.fasta")
 diamond.dbinfo()
 diamond.blastp(
-    query="test/test_proteins.fasta",
-    out="test/test_blastp_output"
+    query="test_proteins.fasta",
+    out="test_blastp_output"
 )
-# diamond.makedb("/home/hongning/AnnoPRO/test_proteins.fasta")
 print("done")
